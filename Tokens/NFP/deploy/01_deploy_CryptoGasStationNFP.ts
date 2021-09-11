7d03815f962243a412f2de94f23173a6d07fdb49
OPPanda <0PP4nd4@gmail.com>
2021-09-11 00:55:29 -0600

    Update deploy to use constants

--- Tokens/NFP/deploy/01_deploy_CryptoGasStationNFP.ts
+++ Tokens/NFP/deploy/01_deploy_CryptoGasStationNFP.ts
@@ -16,16 +16,11 @@ const func: DeployFunction = async function ({
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const usdcAddresses = {
-    '56': '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d',
-    '137': '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
-  };
-
   await deploy('CryptoGasStationNFP', {
     from: deployer,
     args: [
       'https://t2mir7dilg.execute-api.us-west-2.amazonaws.com/dev/tokens/',
-      usdcAddresses[network.config.chainId],
+      USDC_ADDRESSES[network.config.chainId],
       deployer,
     ],
     log: true,
